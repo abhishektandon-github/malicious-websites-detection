cdc1b76c77a7a7dd8b92ab00e67b7389ba2355ea
abhishektandon <abhishektandon0406@gmail.com>
2021-06-06 13:11:27 +0530

    update

--- detect.py
+++ detect.py
@@ -9,31 +9,43 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+# load dataset
 df = pd.read_csv('data/dataset.csv')
 
+# ip and target
 df_ip = df['IP'].values
 target = df['target'].values
 
+
+# separate the 4 octets
 arr = np.array(list(map(lambda x: x.split('.'), df_ip))).astype(np.int)
+
+# convert numpy array `arr` to pandas DataFrame
 
 new_df = pd.DataFrame(data = arr, columns = ['X1', 'X2', 'X3', 'X4'])
 
-# Octet
+# # Feature Extraction
+
+# Adding new features acc to Octet Method
 new_df['N2'] = 256 + new_df['X2']
 new_df['N3'] = 512 + new_df['X3']
 new_df['N4'] = 768 + new_df['X4']
 
-# Ex-Octet
+# Additional features for Ex-Octet Method
 new_df['N5'] = 768 + (new_df['X1']+new_df['X2'])%256
 new_df['N6'] = 1024 + (new_df['X1']+new_df['X2']+new_df['X3'])%256
 
+
 df2 = new_df[['X1', 'N2', 'N3', 'N5', 'N6']].values
 
+
+# Combining malicious and benign data for train and test
 X_train = np.concatenate((df2[:14001], df2[24438:34440]), axis = 0)
 y_train = np.concatenate((target[:14001], target[24438:34440]), axis = 0)
 
 X_test = np.concatenate((df2[10002:11002], df2[34441:49441]), axis = 0)
 y_test = np.concatenate((target[10002:11002], target[34441:49441]), axis = 0)
+
 
 
 model = RandomForestClassifier()
